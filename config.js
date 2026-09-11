466e3493ab1bd87108fbe124b69c5ff2c4bad76e
dontonburi <100891326+dontonburi@users.noreply.github.com>
2026-09-11 16:46:34 -0400

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-     SUPABASE_URL: https://lhisvzcgvznsginzhsgn.supabase.co,
-     SUPABASE_ANON_KEY: sb_publishable_TrLAYn2Wx2slp67suqZfMA_LPRBdHKh,
+     SUPABASE_URL: "https://lhisvzcgvznsginzhsgn.supabase.co",
+     SUPABASE_ANON_KEY: "sb_publishable_TrLAYn2Wx2slp67suqZfMA_LPRBdHKh",
    };
