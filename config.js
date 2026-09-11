173b5f8f930e28c75f8426801de098c9cb2a95da
dontonburi <100891326+dontonburi@users.noreply.github.com>
2026-09-11 16:56:56 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-     SUPABASE_URL: "https://lhisvzcgvznsginzhsgn.supabase.co",
-     SUPABASE_ANON_KEY: "sb_publishable_TrLAYn2Wx2slp67suqZfMA_LPRBdHKh",
-   };
+  SUPABASE_URL: "https://lhisvzcgvznsginzhsgn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_TrLAYn2Wx2s1p67suqZfMA_LPRBdHKh",
+  LOGIN_EMAIL: "chindonsang@gmail.com",  
+};
