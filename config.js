397687812049a69104dd49eb9ea6789feb62ad1c
dontonburi <100891326+dontonburi@users.noreply.github.com>
2026-09-11 17:10:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   SUPABASE_URL: "https://lhisvzcgvznsginzhsgn.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_TrLAYn2Wx2s1p67suqZfMA_LPRBdHKh",
+  SUPABASE_ANON_KEY: "sb_publishable_TrLAYn2Wx2slp67suqZfMA_LPRBdHKh",
   LOGIN_EMAIL: "",
   REQUIRE_LOGIN: false,   // set to true later to turn the password back on
 };
